6b78aac0de07d6ec61abc42648a0e073a4c085d0
Prosperent <Prosperent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-13 17:25:53 +0000

    this plugin is no longer being updated, please download the Prosperent Suite instead.
    git-svn-id: https://plugins.svn.wordpress.org/prosperent-prosperlinks/tags/1.2@874883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- prosperLinks.php
+++ prosperLinks.php
@@ -30,7 +30,7 @@ add_action( 'admin_notices', 'prosperProspLinksNoticeWrite' );
 function prosperProspLinksNoticeWrite() 
 {
 	echo '<div class="error" style="padding:6px 0;">';
-	echo _e( '<span style="font-size:14px; padding-left:10px;"><b>The Prosperent ProsperLinks plugin is not being update any longer.<b></span><span style="font-size:12px;"></span><br><br>', 'my-text-domain' );
+	echo _e( '<span style="font-size:14px; padding-left:10px;"><b>The Prosperent ProsperLinks plugin is no longer being updated.<b></span><span style="font-size:12px;"></span><br><br>', 'my-text-domain' );
 	echo _e( '<span style="font-size:14px; padding-left:10px;">Please download the new Prosperent plugin instead- <a href="http://wordpress.org/extend/plugins/prosperent-suite/">Prosperent Suite</a></span><span style="font-size:12px;"></span><br><br>', 'my-text-domain' );
 	echo _e( '<span style="font-size:14px; padding-left:10px;">It contains the Prosperent ProsperLinks plus more great tools.</span><span style="font-size:12px;"></span>', 'my-text-domain' );
 	echo '</div>';	
